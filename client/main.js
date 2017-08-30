3e12dec663c25cdc70727907950fc58d0ad4e40e
fuzzybabybunny <victor@victorlinphoto.com>
2017-08-29 20:11:04 -0400

    seeded player data, used DB data to output JSX

--- client/main.js
+++ client/main.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
-
-const players = [{
-	_id: 1,
-	name: 'Fred',
-	score: 22
-},{
-	_id: 2,
-	name: 'Tom',
-	score: -83
-},{
-	_id: 3,
-	name: 'Susan',
-	score: 100
-}];
+import {Mongo} from 'meteor/mongo';
+import {Tracker} from 'meteor/tracker';
+import {Players} from './../imports/api/players';
 
 const renderPlayers = function(playersList){
 	return jsxList = playersList.map(function(player){
@@ -23,16 +12,27 @@ const renderPlayers = function(playersList){
 };
 
 Meteor.startup(function(){
-	let title = "ScoreKeeper!";
-	let name = 'Victor';
 
-	let jsx = (
-		<div>
-			<h1>{title}</h1>
-			<p>Hello, {name}!</p>
-			<p>Second paragraph.</p>
-			{renderPlayers(players)}
-		</div>
-	);
-	ReactDOM.render(jsx, document.getElementById('app'));
+	Tracker.autorun(function(){
+		Meteor.subscribe('players');
+		let title = "ScoreKeeper!";
+		let name = 'Victor';
+		let players = Players.find().fetch();
+
+		let jsx = (
+			<div>
+				<h1>{title}</h1>
+				<p>Hello, {name}!</p>
+				<p>Second paragraph.</p>
+				{renderPlayers(players)}
+			</div>
+		);
+		ReactDOM.render(jsx, document.getElementById('app'));
+
+	});
+
+	Players.insert({
+		name: 'Ted',
+		score: 13
+	});
 });
--- server/main.js
+++ server/main.js
@@ -1,7 +1,30 @@
-import {greetUser, name} from './../imports/utils.js';
-import someAdd from './../imports/math.js';
+import {Meteor} from 'meteor/meteor';
 
-console.log('log from /client/server/main.js');
-console.log(greetUser());
-console.log(name);
-console.log(someAdd(4, 9));
+import {Players} from './../imports/api/players';
+
+Meteor.startup(function(){
+
+	Players.remove({});
+
+	const playersArray = [{
+		name: 'Fred',
+		score: 22
+	},{
+		name: 'Tom',
+		score: -83
+	},{
+		name: 'Susan',
+		score: 100
+	}];
+
+	playersArray.forEach(doc => {
+		Players.insert(doc);
+	})
+
+	console.log(Players.find().fetch());
+
+	Meteor.publish('players', function(){
+		return Players.find();
+	});
+
+});
